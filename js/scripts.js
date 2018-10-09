089a1976027c4a5d5cf397d0f53f68cb8d8d4bf1
estudioduplex <estudioduplex@gmail.com>
2018-10-08 22:18:13 -0300

    Fixes #3 cambios en estilo y script

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 let slides = document.querySelectorAll(".foto");
+let bolitos = document.querySelectorAll(".bolito");
 let height = window.innerHeight;
 let offset = height; 
 
 slides.forEach(function (slide, index) {
 
-    // slide.style.display = "none";
-
     window.addEventListener("scroll", () => {
         console.log("window.pageYOffset:", window.pageYOffset)
         if (window.pageYOffset >= offset + height * 2 * index) {
+            bolitos.forEach(b => b.style.fontSize = "24px");
+            bolitos[index].style.fontSize = "48px"
             slide.style.display = "block";
         }
         else {
