d7239474afff813a796308c711127b41cfeb4514
estudioduplex <estudioduplex@gmail.com>
2018-10-08 21:07:43 -0300

    Fixes #1 #2 El slide de los creditos finales debe tener una imagen sino se desincroniza.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,17 @@
 let slides = document.querySelectorAll(".foto");
 let height = window.innerHeight;
+let offset = document.querySelector("#offset").clientHeight - height; 
 
 slides.forEach(function (slide, index) {
 
     // slide.style.display = "none";
 
     window.addEventListener("scroll", () => {
-
-        // let breakpoint = 2 * height * (index)
-
-        // console.log("index: ", index)
-
-        if (window.pageYOffset > 2 * height * (index + 1)) {
+        console.log("window.pageYOffset:", window.pageYOffset)
+        if (window.pageYOffset >= offset + height * 2 * index) {
             slide.style.display = "block";
         }
-        else if (window.pageYOffset < 2 * height * (index + 1)) {
+        else {
             slide.style.display = "none";
         }
     })
