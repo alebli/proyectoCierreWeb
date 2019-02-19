f161b5101132690c618c8b6bffda9fef57bfa7fd
Carlos Araujo <infocarlosaraujo@gmail.com>
2019-02-19 14:27:11 -0300

    Fix bolitos...

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,46 @@
-window.addEventListener("load", function () {
-    let slides = document.querySelectorAll(".foto");
-    let bolitos = document.querySelectorAll(".bolito");
-    let height = window.innerHeight;
-    let offset = height;
-    let circChico = "24px";
-    let circGrande = "48px";
+window.addEventListener("load", function() {
+    // Crear un bullet x cada foto
+    const fotos = document.querySelectorAll(".foto");
+    fotos.forEach(foto => createBullet());
+    const bolitos = document.querySelectorAll(".bolito");
 
-    bolitos[0].style.fontSize = circGrande;
-
-    slides.forEach(function (slide, index) {
-
+    fotos.forEach((foto, index) => {
         window.addEventListener("scroll", () => {
-            if (window.pageYOffset >= offset + height * 2 * index) {
-                bolitos.forEach(b => b.style.fontSize = circChico);
-                bolitos[index].style.fontSize = circGrande
-                slide.style.display = "block";
-            }
-            else {
-                slide.style.display = "none";
-            }
-        })
+            let height = window.innerHeight;
 
+            if (window.pageYOffset >= height + 2 * height * index) {
+                activarBullet(bolitos, index);
+                mostrarFoto(foto);
+            } else {
+                ocultarFoto(foto);
+                // Achica el primer  bullet[0] en la intro
+                if (index == 0) desactivarBullet(bolitos, 0);
+            }
+        });
     });
 });
+
+function activarBullet(bolitos, index) {
+    bolitos.forEach(b => b.classList.remove("bolitoBig"));
+    bolitos[index].classList.toggle("bolitoBig");
+}
+
+function desactivarBullet(bolitos, index) {
+    bolitos[index].classList.remove("bolitoBig");
+}
+
+function mostrarFoto(foto) {
+    foto.style.display = "block";
+}
+
+function ocultarFoto(foto) {
+    foto.style.display = "none";
+}
+
+function createBullet() {
+    let divNode = document.createElement("div");
+    let divText = document.createTextNode("•");
+    divNode.classList.add("bolito");
+    divNode.appendChild(divText);
+    document.querySelector(".circulos").appendChild(divNode);
+}
